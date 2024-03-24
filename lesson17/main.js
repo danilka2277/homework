8ab82f69b1baa6e6335fbc73e3e972b42fd2d57e
tessia <82707659+danilka2277@users.noreply.github.com>
2024-03-24 20:38:01 +0300

    Add files via upload

---
+++ lesson17/main.js
@@ -0,0 +1,27 @@
+let firstname = 'Daniel'
+let surname = 'Combojo'
+let age = '19 years '
+let height = '175 cm '
+let level = 'level 999'
+
+
+
+console.log (firstname)
+console.log (surname)
+console.log (age)
+console.log (height)
+console.log (level)
+
+// ----------------------------------------------------------------//
+
+const x = '5'
+const y = '10'
+const result = x * y
+const division = y / x
+const sum = 5 + 10
+const minus = x - y
+
+console.log (result)
+console.log (division)
+console.log (sum)
+console.log (minus)
