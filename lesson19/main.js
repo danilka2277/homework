9afbfb19857807920e65a8660c84b1d11a21847b
tessia <82707659+danilka2277@users.noreply.github.com>
2024-06-22 20:03:32 +0300

    Add files via upload

---
+++ lesson19/main.js
@@ -0,0 +1,21 @@
+let i = 11
+let u = 0
+
+while(i > 1) {
+    i = i - 1
+    console.log(i)
+}
+
+while(u <= 10) {
+    u = u + 2
+    console.log(u)
+}
+
+
+for (let i = 2; i <= 12; i = i + 2 ) {
+    console.log(`Четные числа ${i} `)
+}
+
+for (let i = 10; i >= 1; i = i - 1 ) {
+    console.log(`Обратный порядок ${i} `)
+}
